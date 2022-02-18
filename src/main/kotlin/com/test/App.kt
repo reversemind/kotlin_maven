3bf3a2b44ea8b69b3b6572ba73754edf48a01f26
Eugene Kalinin <kalinin.eugene@gmail.com>
2022-02-18 10:05:40 +0300

    Initial commit

---
+++ src/main/kotlin/com/test/App.kt
@@ -0,0 +1,8 @@
+package com.test
+
+object App {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello, World!")
+    }
+}
---
+++ src/test/kotlin/com/test/AppTest.kt
@@ -0,0 +1,12 @@
+package com.test
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AppTest {
+
+    @Test
+    fun shouldAnswerWithTrue() {
+        assertTrue(true)
+    }
+}
